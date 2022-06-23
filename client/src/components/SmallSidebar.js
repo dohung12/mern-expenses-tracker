1728bc3bcfa9777be36cbd784d36526e7a323760
dohung <dohuyhung12@gmail.com>
2022-06-23 12:45:20 +0700

    add small screen sidebar

--- client/src/components/SmallSidebar.js
+++ client/src/components/SmallSidebar.js
@@ -1,7 +1,38 @@
 import React from 'react';
+import Wrapper from '../assets/Wrapper/SmallSidebarWrapper';
+import { useAppContext } from '../context/appContext';
+import { useToggleSidebar } from '../hooks';
+import { FaTimes } from 'react-icons/fa';
+
+import SidebarLinks from './SidebarLinks';
+import Logo from './Logo';
 
 const SmallSidebar = () => {
-  return <div>SmallSidebar</div>;
+  const { state } = useAppContext();
+  const toggleSidebar = useToggleSidebar();
+  return (
+    <Wrapper>
+      <div
+        className={
+          state.showSidebar
+            ? 'sidebar-container show-sidebar'
+            : 'sidebar-container'
+        }
+      >
+        <div className='content'>
+          <button className='close-btn' onClick={toggleSidebar}>
+            <FaTimes />
+          </button>
+          <header>
+            <Logo />
+          </header>
+          <div className='nav-links'>
+            <SidebarLinks toggleSidebar={toggleSidebar} />
+          </div>
+        </div>
+      </div>
+    </Wrapper>
+  );
 };
 
 export default SmallSidebar;
