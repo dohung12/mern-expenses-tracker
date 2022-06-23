5ccb1e1bb8140d7bf2ecfcb258277284e2993e71
dohung <dohuyhung12@gmail.com>
2022-06-23 13:02:11 +0700

    add private navbar

---
+++ client/src/assets/Wrapper/PrivateNavbarWrapper.js
@@ -0,0 +1,56 @@
+import styled from 'styled-components';
+
+const Wrapper = styled.nav`
+  box-shadow: rgb(0 0 0 / 10%) 0px 1px 0px 0px;
+  background-color: #fff;
+
+  li {
+    padding: 0;
+    .active {
+      font-weight: bolder;
+      color: var(--primary);
+    }
+  }
+
+  .logo {
+    display: flex;
+    align-items: center;
+    h2 {
+      margin: 0;
+      display: none;
+      @media (min-width: 992px) {
+        display: block;
+      }
+    }
+  }
+
+  .nav-link {
+    display: flex;
+    align-items: center;
+    gap: 0.5rem;
+
+    svg {
+      color: var(--primary);
+    }
+  }
+
+  summary {
+    display: flex;
+    align-items: center;
+
+    h5 {
+      margin: 0 8px;
+      display: none;
+      @media (min-width: 992px) {
+        display: block;
+      }
+    }
+    :focus {
+      background-color: #00897b;
+      h5 {
+        color: white;
+      }
+    }
+  }
+`;
+export default Wrapper;
--- client/src/components/PrivateNavbar.js
+++ client/src/components/PrivateNavbar.js
@@ -1,7 +1,68 @@
-import React from 'react';
+import { NavLink } from 'react-router-dom';
+import { FaAlignLeft } from 'react-icons/fa';
+import { useAppContext } from '../context/appContext';
+import { useToggleSidebar } from '../hooks/index';
 
-const PrivateNavbar = () => {
-  return <div>PrivateNavbar</div>;
+// import LogoutBtn from './LogoutBtn';
+// import Avatar from './Avatar';
+// import SearchForm from './SearchForm';
+import Logo from './Logo';
+
+import Wrapper from '../assets/Wrapper/PrivateNavbarWrapper';
+import links from '../utils/links';
+const PrivNavBar = () => {
+  const { state } = useAppContext();
+  const { username } = state.user;
+
+  const toggleSidebar = useToggleSidebar();
+
+  return (
+    <Wrapper className='container-fluid'>
+      <ul>
+        <li>
+          <div
+            role={'button'}
+            className='outline contrast'
+            onClick={toggleSidebar}
+          >
+            <FaAlignLeft />
+          </div>
+        </li>
+        <li className='logo'>
+          <Logo />
+          <a href='/'>
+            <h2>Where's My Money</h2>
+          </a>
+        </li>
+      </ul>
+      {/* <SearchForm /> */}
+      <ul>
+        <li>
+          <details role={'list'} dir='rtl'>
+            <summary aria-haspopup='listbox' role={'link'}>
+              {/* <Avatar src={state.user.profilePic} /> */}
+              <h5>{username}</h5>
+            </summary>
+            <ul role={'listbox'}>
+              {links.map((link) => {
+                const { id, text, path, icon } = link;
+                return (
+                  <li key={id} className='nav-link'>
+                    {icon}
+                    <NavLink to={path}>{text}</NavLink>
+                  </li>
+                );
+              })}
+
+              {/* <li>
+                <LogoutBtn />
+              </li> */}
+            </ul>
+          </details>
+        </li>
+      </ul>
+    </Wrapper>
+  );
 };
 
-export default PrivateNavbar;
+export default PrivNavBar;
