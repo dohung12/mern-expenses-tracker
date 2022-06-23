244f85b3155d82c28c654f4ecc0b4189b3423a31
dohung <dohuyhung12@gmail.com>
2022-06-23 09:24:49 +0700

    import css

--- client/src/index.js
+++ client/src/index.js
@@ -2,6 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
+import 'normalize.css';
+import '@picocss/pico';
+import './assets/css/style.css';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
