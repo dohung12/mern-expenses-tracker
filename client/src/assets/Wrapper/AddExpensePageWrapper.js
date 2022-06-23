2db036bb8763ea60da281a021e0a444b7943a270
dohung <dohuyhung12@gmail.com>
2022-06-23 17:19:37 +0700

    add newExpense page

---
+++ client/src/assets/Wrapper/AddExpensePageWrapper.js
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+  padding: 3rem;
+  border-radius: 0.5rem;
+  background-color: #fff;
+
+  label {
+    text-transform: capitalize;
+  }
+
+  .grid {
+    grid-template-columns: repeat(auto-fit, minmax(250px, 1fr));
+  }
+`;
+
+export default Wrapper;
--- client/src/pages/AddExpense.js
+++ client/src/pages/AddExpense.js
@@ -1,7 +1,142 @@
-import React from 'react';
+import { useState } from 'react';
+import { FormRow, Alert } from '../components/';
+import { useAlert, useAuthFetch } from '../hooks/index';
+import Wrapper from '../assets/Wrapper/AddExpensePageWrapper';
+
+const initState = {
+  title: '',
+  category: '',
+  amount: 0,
+  incurred_on_date: '',
+  incurred_on_time: '',
+  notes: '',
+};
 
 const AddExpense = () => {
-  return <div>AddExpense</div>;
+  const [values, setValues] = useState(initState);
+  const { title, category, amount, notes, incurred_on_date, incurred_on_time } =
+    values;
+
+  const [alert, displayAlert] = useAlert();
+  const { showAlert, alertType, alertText } = alert;
+  const [isLoading, setIsLoading] = useState(false);
+
+  const authFetch = useAuthFetch();
+
+  const clearForm = () => {
+    setValues(initState);
+  };
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setValues({
+      ...values,
+      [name]: value,
+    });
+  };
+
+  const createExpense = async () => {
+    setIsLoading(true);
+    try {
+      await authFetch.post('/expenses', {
+        title,
+        category,
+        amount,
+        notes,
+        incurred_on: `${incurred_on_date} ${incurred_on_time}`,
+      });
+      displayAlert('Create New Expense Successful', 'success');
+      clearForm();
+    } catch (error) {
+      if (error.response.status === 401) {
+        return;
+      }
+      displayAlert(error.response.data.msg, 'danger');
+    }
+    setIsLoading(false);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (!title || !amount || !category) {
+      displayAlert('Please provide all required values', 'danger');
+    } else {
+      console.log(values);
+      createExpense();
+    }
+  };
+
+  return (
+    <Wrapper>
+      <form action='' onSubmit={handleSubmit}>
+        <div className='grid'>
+          <FormRow
+            name={'title'}
+            handleChange={handleChange}
+            placeholder='title'
+            type={'text'}
+            key='title'
+            value={title}
+            required
+          />
+          <FormRow
+            name={'category'}
+            handleChange={handleChange}
+            placeholder='category'
+            type={'text'}
+            key='category'
+            value={category}
+            required
+          />
+          <FormRow
+            name={'amount'}
+            handleChange={handleChange}
+            placeholder='amount'
+            type={'number'}
+            key='amount'
+            value={amount}
+            required
+          />
+        </div>
+
+        <div className='grid'>
+          <FormRow
+            name={'incurred_on_date'}
+            handleChange={handleChange}
+            placeholder='incurred on date'
+            labelText={'Date'}
+            type={'date'}
+            key='incurred_on_date'
+            value={incurred_on_date}
+          />
+          <FormRow
+            name={'incurred_on_time'}
+            handleChange={handleChange}
+            placeholder='incurred on time'
+            labelText={'time'}
+            type={'time'}
+            key='incurred_on_time'
+            value={incurred_on_time}
+          />
+        </div>
+        <FormRow
+          name={'notes'}
+          handleChange={handleChange}
+          placeholder='notes'
+          type={'text'}
+          key='notes'
+          value={notes}
+        />
+        <Alert />
+        {showAlert && <Alert alertText={alertText} alertType={alertType} />}
+
+        <button type='submit' disabled={isLoading}>
+          Submit
+        </button>
+      </form>
+    </Wrapper>
+  );
 };
 
 export default AddExpense;
