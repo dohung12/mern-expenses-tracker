853f7ba8b7ddeb47e60f67baac43f7b70157e3c5
dohung <dohuyhung12@gmail.com>
2022-06-23 09:47:00 +0700

    add routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,16 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { Landing, Error, Register, Login } from './pages/';
 function App() {
-  return <div className='App'>Hello world</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/landing' element={<Landing />}></Route>
+        <Route path='/login' element={<Login />}></Route>
+        <Route path='/register' element={<Register />}></Route>
+        <Route path='*' element={<Error />}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
---
+++ client/src/pages/index.js
@@ -0,0 +1,6 @@
+import Landing from './Landing';
+import Register from './Register';
+import Error from './Error';
+import Login from './Login';
+
+export { Landing, Register, Error, Login };
