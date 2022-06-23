7c509c993f3dcf6e7c9dca73666cefc0011527a7
dohung <dohuyhung12@gmail.com>
2022-06-23 10:33:17 +0700

    set up global context

---
+++ client/src/context/reducer.js
@@ -0,0 +1,31 @@
+function reducer(state, action) {
+  switch (action.type) {
+    case 'SETUP_USER':
+      const { user, token } = action.payload;
+      return {
+        ...state,
+        user,
+        token,
+      };
+    case 'LOGOUT_USER':
+      return {
+        ...state,
+        user: null,
+        token: null,
+      };
+    case 'TOGGLE_SIDEBAR':
+      return {
+        ...state,
+        showSidebar: !state.showSidebar,
+      };
+    case 'UPDATE_USER':
+      return {
+        ...state,
+        user: action.payload.user,
+      };
+    default:
+      throw new Error(`Action type: ${action.type} does not exist`);
+  }
+}
+
+export default reducer;
--- client/src/index.js
+++ client/src/index.js
@@ -6,9 +6,13 @@ import 'normalize.css';
 import '@picocss/pico';
 import './assets/css/style.css';
 
+import { AppProvider } from './context/appContext';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <AppProvider>
+      <App />
+    </AppProvider>
   </React.StrictMode>
 );
