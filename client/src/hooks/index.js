25bc4992f39d518ad21b5720289e7bb897a02201
dohung <dohuyhung12@gmail.com>
2022-06-23 15:26:47 +0700

    add fetch for auth route

--- client/src/hooks/index.js
+++ client/src/hooks/index.js
@@ -2,4 +2,11 @@ import useAlert from './useAlert';
 import useSetupUser from './useSetupUser';
 import useToggleSidebar from './useToggleSidebar';
 import useLogoutUser from './useLogoutUser';
-export { useAlert, useSetupUser, useToggleSidebar, useLogoutUser };
+import useAuthFetch from './useAuthFetch';
+export {
+  useAlert,
+  useSetupUser,
+  useToggleSidebar,
+  useLogoutUser,
+  useAuthFetch,
+};
