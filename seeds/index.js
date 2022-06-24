f4b18cebc8950833bcc3303838986c747135c0f3
dohung <dohuyhung12@gmail.com>
2022-06-24 14:55:56 +0700

    misc

--- seeds/index.js
+++ seeds/index.js
@@ -2,11 +2,36 @@ require('dotenv').config();
 const mongoose = require('mongoose');
 const { faker } = require('@faker-js/faker');
 const ExpenseSchema = require('../models/expense.model');
+const CategorySchema = require('../models/category.model');
+
+// const categoryId = [
+//   new ObjectId('62b53481d55cd656f0d87de7'),
+//   new ObjectId('62b53482d55cd656f0d87de9'),
+//   new ObjectId('62b53482d55cd656f0d87deb'),
+//   new ObjectId('62b53482d55cd656f0d87ded'),
+//   new ObjectId('62b53482d55cd656f0d87def'),
+//   new ObjectId('62b53482d55cd656f0d87df1'),
+//   new ObjectId('62b53482d55cd656f0d87df3'),
+//   new ObjectId('62b53482d55cd656f0d87df5'),
+//   new ObjectId('62b53482d55cd656f0d87df7'),
+// ];
+
+const categoryId = [
+  '62b53481d55cd656f0d87de7',
+  '62b53482d55cd656f0d87de9',
+  '62b53482d55cd656f0d87deb',
+  '62b53482d55cd656f0d87ded',
+  '62b53482d55cd656f0d87def',
+  '62b53482d55cd656f0d87df1',
+  '62b53482d55cd656f0d87df3',
+  '62b53482d55cd656f0d87df5',
+  '62b53482d55cd656f0d87df7',
+];
 
 const createRandomExpense = () => {
   return {
     title: faker.lorem.sentence(),
-    category: faker.helpers.arrayElement(['meals', 'groceries', 'commute']),
+    category: faker.helpers.arrayElement(categoryId),
     amount: faker.finance.amount(),
     incurred_on: faker.date.past(),
     notes: faker.lorem.words(),
@@ -14,12 +39,20 @@ const createRandomExpense = () => {
   };
 };
 
-const expenseAmount = 10;
+// const createDbData = async () => {
+//   const expenseAmount = 10;
+//   for (let index = 0; index < expenseAmount; index++) {
+//     const expense = createRandomExpense();
+//     await ExpenseSchema.create(expense);
+//   }
+// };
 
 const start = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI);
 
+    await ExpenseSchema.deleteMany();
+    const expenseAmount = 10;
     for (let index = 0; index < expenseAmount; index++) {
       const expense = createRandomExpense();
       await ExpenseSchema.create(expense);
