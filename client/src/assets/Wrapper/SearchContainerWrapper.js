8acd001e3328d754ab7356aba77573567627674b
dohung <dohuyhung12@gmail.com>
2022-06-24 23:10:29 +0700

    update search container

---
+++ client/src/assets/Wrapper/SearchContainerWrapper.js
@@ -0,0 +1,35 @@
+import styled from 'styled-components';
+const Wrapper = styled.details`
+  padding: 2rem 2.5rem;
+  background-color: #fff;
+  box-shadow: 0 10px 15px -3px rgb(0 0 0 / 0.1), 0 4px 6px -4px rgb(0 0 0 / 0.1);
+  border-radius: 5px;
+  margin-bottom: 2rem;
+
+  summary {
+    border-bottom: 2px solid var(--primary);
+    .header {
+      display: inline-block;
+      margin-bottom: 1rem;
+    }
+    ::after {
+      background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 512 512'%3E%3C!--! Font Awesome Pro 6.1.1 by %40fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons  Inc. --%3E%3Cpath d='M256 0C114.6 0 0 114.6 0 256c0 141.4 114.6 256 256 256s256-114.6 256-256C512 114.6 397.4 0 256 0zM358.6 278.6l-112 112c-12.5 12.5-32.75 12.5-45.25 0s-12.5-32.75 0-45.25L290.8 256L201.4 166.6c-12.5-12.5-12.5-32.75 0-45.25s32.75-12.5 45.25 0l112 112C364.9 239.6 368 247.8 368 256S364.9 272.4 358.6 278.6z'/%3E%3C/svg%3E");
+      background-size: 1.5rem auto;
+      width: 1.5rem;
+      height: 1.5rem;
+    }
+  }
+
+  label,
+  option,
+  select {
+    text-transform: capitalize;
+  }
+
+  .header,
+  form {
+    margin-bottom: 0;
+  }
+`;
+
+export default Wrapper;
--- client/src/components/SearchContainer.js
+++ client/src/components/SearchContainer.js
@@ -1,45 +1,9 @@
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import { FormRow, Alert } from './index';
 import { useAppContext } from '../context/appContext';
-import styled from 'styled-components';
-import {
-  useAlert,
-  useAuthFetch,
-  useSetupExpenses,
-  useGetExpenses,
-} from '../hooks';
-const Wrapper = styled.details`
-  padding: 2rem 2.5rem;
-  background-color: #fff;
-  box-shadow: 0 10px 15px -3px rgb(0 0 0 / 0.1), 0 4px 6px -4px rgb(0 0 0 / 0.1);
-  border-radius: 5px;
-  margin-bottom: 2rem;
+import { useAlert, useGetExpenses } from '../hooks';
+import Wrapper from '../assets/Wrapper/SearchContainerWrapper';
 
-  summary {
-    border-bottom: 2px solid var(--primary);
-    .header {
-      display: inline-block;
-      margin-bottom: 1rem;
-    }
-    ::after {
-      background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 512 512'%3E%3C!--! Font Awesome Pro 6.1.1 by %40fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons  Inc. --%3E%3Cpath d='M256 0C114.6 0 0 114.6 0 256c0 141.4 114.6 256 256 256s256-114.6 256-256C512 114.6 397.4 0 256 0zM358.6 278.6l-112 112c-12.5 12.5-32.75 12.5-45.25 0s-12.5-32.75 0-45.25L290.8 256L201.4 166.6c-12.5-12.5-12.5-32.75 0-45.25s32.75-12.5 45.25 0l112 112C364.9 239.6 368 247.8 368 256S364.9 272.4 358.6 278.6z'/%3E%3C/svg%3E");
-      background-size: 1.5rem auto;
-      width: 1.5rem;
-      height: 1.5rem;
-    }
-  }
-
-  label,
-  option,
-  select {
-    text-transform: capitalize;
-  }
-
-  .header,
-  form {
-    margin-bottom: 0;
-  }
-`;
 const initState = {
   title: '',
   category: 'all',
@@ -60,9 +24,8 @@ const sortOptions = [
   'amount: high to low',
 ];
 
-const SearchContainer = () => {
+const SearchContainer = ({ setNumOfPages }) => {
   const { state } = useAppContext();
-  const [isLoading, setIsLoading] = useState(false);
   const [values, setValues] = useState(initState);
   const {
     title,
@@ -77,8 +40,6 @@ const SearchContainer = () => {
   } = values;
 
   const getExpenses = useGetExpenses();
-  const setupExpenses = useSetupExpenses();
-  const authFetch = useAuthFetch();
   const categoryOptions = state.categories.map((category) => {
     return category.title;
   });
@@ -95,17 +56,6 @@ const SearchContainer = () => {
   const handleClearBtn = (e) => {
     e.preventDefault();
     setValues(initState);
-  };
-  const fetchQuery = async (url) => {
-    setIsLoading(true);
-    try {
-      const { data } = await authFetch.get(url);
-      setupExpenses(data.expenses);
-      setValues(initState);
-    } catch (error) {
-      console.log(error);
-    }
-    setIsLoading(false);
   };
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -150,13 +100,9 @@ const SearchContainer = () => {
     } else if (amountFrom && amountTo && amountFrom > amountTo) {
       displayAlert('Starting amount must not larger than end amount', 'danger');
     } else {
-      fetchQuery(url);
+      getExpenses(url);
     }
   };
-
-  useEffect(() => {
-    getExpenses();
-  }, []);
 
   return (
     <Wrapper>
@@ -272,13 +218,7 @@ const SearchContainer = () => {
           <button className='contrast' onClick={handleClearBtn}>
             Clear Filter
           </button>
-          <button
-            onClick={handleSubmit}
-            disabled={isLoading}
-            aria-busy={isLoading}
-          >
-            {isLoading ? '' : 'Search'}
-          </button>
+          <button onClick={handleSubmit}>Search</button>
         </div>
       </form>
     </Wrapper>
