0f6cfeb1652140fad19eec811815067ee487182b
dohung <dohuyhung12@gmail.com>
2022-06-24 09:47:38 +0700

    styling

--- client/src/assets/Wrapper/SingleExpenseBlockWrapper.js
+++ client/src/assets/Wrapper/SingleExpenseBlockWrapper.js
@@ -5,15 +5,28 @@ const Wrapper = styled.details`
   padding: 1rem;
   border-radius: 0.5rem;
   summary {
-    display: grid;
-    grid-template-columns: 200px 1fr auto;
+    display: flex;
+    justify-content: space-between;
     gap: 1rem;
+    flex-direction: column;
+
+    @media (min-width: 600px) {
+      flex-direction: row;
+    }
+
     .title {
+      width: 100%;
       margin-bottom: 0;
+      align-self: flex-end;
     }
     .amount {
       color: var(--primary);
       margin-bottom: 0;
+
+      @media (min-width: 600px) {
+        width: 200px;
+        flex-direction: row;
+      }
     }
   }
 
@@ -23,12 +36,13 @@ const Wrapper = styled.details`
     border-top: 1px solid #ccc;
     padding-top: 1rem;
     ul {
-      margin-bottom: 0;
+      margin: auto 0;
     }
     .btn-container {
       display: flex;
       justify-content: flex-end;
       align-items: flex-end;
+      flex-direction: column;
       gap: 0.5rem;
 
       div {
