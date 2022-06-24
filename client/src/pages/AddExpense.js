9f5051aa758306baf7d7e9a583a3a160d7ddbafa
dohung <dohuyhung12@gmail.com>
2022-06-24 13:37:54 +0700

    add new category modal

--- client/src/pages/AddExpense.js
+++ client/src/pages/AddExpense.js
@@ -1,7 +1,8 @@
-import { useState } from 'react';
-import { FormRow, Alert } from '../components/';
-import { useAlert, useAuthFetch } from '../hooks/index';
+import { useState, useEffect } from 'react';
+import { FormRow, Alert, Dropdown, AddCategoryModal } from '../components/';
+import { useAlert, useAuthFetch, useGetCategories } from '../hooks/index';
 import Wrapper from '../assets/Wrapper/AddExpensePageWrapper';
+import { useAppContext } from '../context/appContext';
 
 const initState = {
   title: '',
@@ -10,6 +11,7 @@ const initState = {
   incurred_on_date: '',
   incurred_on_time: '',
   notes: '',
+  showModal: false,
 };
 
 const AddExpense = () => {
@@ -17,16 +19,14 @@ const AddExpense = () => {
   const { title, category, amount, notes, incurred_on_date, incurred_on_time } =
     values;
 
+  const { state } = useAppContext();
   const [alert, displayAlert] = useAlert();
   const { showAlert, alertType, alertText } = alert;
   const [isLoading, setIsLoading] = useState(false);
+  const getCategories = useGetCategories();
 
   const authFetch = useAuthFetch();
-
-  const clearForm = () => {
-    setValues(initState);
-  };
-
+  //  CREATE NEW EXPENSE CONTROLLER
   const handleChange = (e) => {
     const { name, value } = e.target;
     setValues({
@@ -34,7 +34,6 @@ const AddExpense = () => {
       [name]: value,
     });
   };
-
   const createExpense = async () => {
     setIsLoading(true);
     try {
@@ -46,7 +45,8 @@ const AddExpense = () => {
         incurred_on: `${incurred_on_date} ${incurred_on_time}`,
       });
       displayAlert('Create New Expense Successful', 'success');
-      clearForm();
+      // CLEAR FORM
+      setValues(initState);
     } catch (error) {
       if (error.response.status === 401) {
         return;
@@ -55,10 +55,8 @@ const AddExpense = () => {
     }
     setIsLoading(false);
   };
-
   const handleSubmit = (e) => {
     e.preventDefault();
-
     if (!title || !amount || !category) {
       displayAlert('Please provide all required values', 'danger');
     } else {
@@ -66,75 +64,96 @@ const AddExpense = () => {
     }
   };
 
+  // CREATE NEW CATEGORY CONTROLLER
+  const toggleModal = () => {
+    setValues({
+      ...values,
+      showModal: !values.showModal,
+    });
+  };
+
+  // FETCH CATEGORIES WHEN LOADING
+  useEffect(() => {
+    getCategories();
+  }, []);
+
   return (
-    <Wrapper>
-      <form action='' onSubmit={handleSubmit}>
-        <div className='grid'>
-          <FormRow
-            name={'title'}
-            handleChange={handleChange}
-            placeholder='title'
-            type={'text'}
-            key='title'
-            value={title}
-            required
-          />
-          <FormRow
-            name={'category'}
-            handleChange={handleChange}
-            placeholder='category'
-            type={'text'}
-            key='category'
-            value={category}
-            required
-          />
-        </div>
-
-        <div className='grid'>
-          <FormRow
-            name={'amount'}
-            handleChange={handleChange}
-            placeholder='amount'
-            type={'number'}
-            key='amount'
-            value={amount}
-            required
-          />
-          <FormRow
-            name={'incurred_on_date'}
-            handleChange={handleChange}
-            placeholder='incurred on date'
-            labelText={'Date'}
-            type={'date'}
-            key='incurred_on_date'
-            value={incurred_on_date}
-          />
-          <FormRow
-            name={'incurred_on_time'}
-            handleChange={handleChange}
-            placeholder='incurred on time'
-            labelText={'time'}
-            type={'time'}
-            key='incurred_on_time'
-            value={incurred_on_time}
-          />
-        </div>
-        <FormRow
-          name={'notes'}
-          handleChange={handleChange}
-          placeholder='notes'
-          type={'text'}
-          key='notes'
-          value={notes}
-        />
-        <Alert />
+    <>
+      <Wrapper data-target='add-expense-form'>
         {showAlert && <Alert alertText={alertText} alertType={alertType} />}
+        <form action='' onSubmit={handleSubmit}>
+          <div className='grid'>
+            <FormRow
+              name={'title'}
+              handleChange={handleChange}
+              placeholder='title'
+              type={'text'}
+              key='title'
+              value={title}
+              required
+            />
+            <FormRow
+              name={'amount'}
+              handleChange={handleChange}
+              placeholder='amount'
+              type={'number'}
+              key='amount'
+              value={amount}
+              required
+            />
+          </div>
+          <div className='grid'>
+            <Dropdown
+              optionLists={state.categories}
+              name={'category'}
+              handleChange={handleChange}
+              // value={category}
+            />
+            <FormRow
+              name={'incurred_on_date'}
+              handleChange={handleChange}
+              placeholder='incurred on date'
+              labelText={'Date'}
+              type={'date'}
+              key='incurred_on_date'
+              value={incurred_on_date}
+            />
+            <FormRow
+              name={'incurred_on_time'}
+              handleChange={handleChange}
+              placeholder='incurred on time'
+              labelText={'time'}
+              type={'time'}
+              key='incurred_on_time'
+              value={incurred_on_time}
+            />
+          </div>
+          <FormRow
+            name={'notes'}
+            handleChange={handleChange}
+            placeholder='notes'
+            type={'text'}
+            key='notes'
+            value={notes}
+          />
+          <a href='#newCategory' onClick={toggleModal}>
+            Add new category
+          </a>
+          <button type='submit' disabled={isLoading}>
+            Submit
+          </button>
+        </form>
+      </Wrapper>
 
-        <button type='submit' disabled={isLoading}>
-          Submit
-        </button>
-      </form>
-    </Wrapper>
+      {/* New Category Modal */}
+      {values.showModal && (
+        <AddCategoryModal
+          dataTarget={'add-expense-form'}
+          showModal={values.showModal}
+          toggleModal={toggleModal}
+        />
+      )}
+    </>
   );
 };
 
