8636c039975a5acdd0f3b763fdcbb3f0e68f9cbd
dohung <dohuyhung12@gmail.com>
2022-06-24 13:36:58 +0700

    complete dropdown

--- client/src/components/Dropdown.js
+++ client/src/components/Dropdown.js
@@ -1,16 +1,19 @@
 import React from 'react';
 
-const Dropdown = ({ optionLists }) => {
+const Dropdown = ({ optionLists, name, labelText, handleChange, value }) => {
   return (
-    <select>
-      {optionLists.map((option, index) => {
-        return (
-          <option value={option} key={index}>
-            {option}
-          </option>
-        );
-      })}
-    </select>
+    <div>
+      <label htmlFor={name}>{labelText || name}</label>
+      <select onChange={handleChange} value={value} name={name}>
+        {optionLists.map((option, index) => {
+          return (
+            <option value={option.title} key={index}>
+              {option.title}
+            </option>
+          );
+        })}
+      </select>
+    </div>
   );
 };
 
