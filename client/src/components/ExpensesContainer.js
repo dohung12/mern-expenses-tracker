e90a39f71cbec33271bf9124a7ff2ec8ba72caf1
dohung <dohuyhung12@gmail.com>
2022-06-25 08:41:36 +0700

    move fetch data to  parent, add pagination

--- client/src/components/ExpensesContainer.js
+++ client/src/components/ExpensesContainer.js
@@ -1,27 +1,22 @@
-import { useEffect } from 'react';
 import { useAppContext } from '../context/appContext';
 import { SingleExpense } from './index';
-import { useGetCategories, useGetExpenses } from '../hooks';
+import { PageBtnContainer } from './index';
 import Wrapper from '../assets/Wrapper/ExpensesContainerWrapper';
 
-const ExpensesContainer = () => {
+const ExpensesContainer = ({ page, setPage }) => {
   const { state } = useAppContext();
-
-  const getExpenses = useGetExpenses();
-  const getCategories = useGetCategories();
-
-  useEffect(() => {
-    getExpenses();
-    getCategories();
-  }, []);
-
   return (
     <>
       {state.count === 0 && <h1>No expenses to display</h1>}
+      <h4>
+        {state.count} Expense{state.count > 1 ? 's' : ''} Found
+      </h4>
+      <PageBtnContainer
+        numOfPages={state.numOfPages}
+        page={page}
+        changePage={setPage}
+      />
       <Wrapper>
-        <h4>
-          {state.count} Expense{state.count > 1 ? 's' : ''} Found
-        </h4>
         {state.count > 0 &&
           state.expenses.map((expense) => {
             return <SingleExpense key={expense._id} {...expense} />;
