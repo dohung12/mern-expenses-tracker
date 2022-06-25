fadb8f03d7cae05238eb7bc40a89c9291f54062d
dohung <dohuyhung12@gmail.com>
2022-06-25 08:41:51 +0700

    change limit

--- controllers/expense.controller.js
+++ controllers/expense.controller.js
@@ -103,7 +103,7 @@ const getAllExpense = async (req, res) => {
   }
 
   const page = Number(req.query.page) || 1;
-  const limit = Number(req.query.limit) || 10;
+  const limit = Number(req.query.limit) || 15;
   const skip = (page - 1) * limit;
 
   result = result.skip(skip).limit(limit);
