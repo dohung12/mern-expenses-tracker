c0aee7eef5b5ee502413b62878af6d7c5fe9c151
dohung <dohuyhung12@gmail.com>
2022-06-26 23:43:30 +0700

    add custom label

--- client/src/components/PieChart.js
+++ client/src/components/PieChart.js
@@ -7,6 +7,33 @@ import {
   Cell,
 } from 'recharts';
 
+const RADIAN = Math.PI / 180;
+const renderCustomizedLabel = ({
+  cx,
+  cy,
+  midAngle,
+  innerRadius,
+  outerRadius,
+  percent,
+  index,
+}) => {
+  const radius = innerRadius + (outerRadius - innerRadius) * 0.5;
+  const x = cx + radius * Math.cos(-midAngle * RADIAN);
+  const y = cy + radius * Math.sin(-midAngle * RADIAN);
+
+  return (
+    <text
+      x={x}
+      y={y}
+      fill='white'
+      textAnchor={x > cx ? 'start' : 'end'}
+      dominantBaseline='central'
+    >
+      {`${(percent * 100).toFixed(0)}%`}
+    </text>
+  );
+};
+
 const PieChartComponent = ({ data }) => {
   const COLORS = [
     '#003f5c',
@@ -30,7 +57,7 @@ const PieChartComponent = ({ data }) => {
           cy='50%'
           outerRadius={150}
           fill='#82ca9d'
-          label
+          label={renderCustomizedLabel}
         >
           {data.map((entry, index) => (
             <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
