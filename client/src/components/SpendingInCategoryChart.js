3a0d5b980cbad629f56a475910e5cd9a456cc8e6
dohung <dohuyhung12@gmail.com>
2022-06-26 23:38:17 +0700

    add alert to unlogical setting date

--- client/src/components/SpendingInCategoryChart.js
+++ client/src/components/SpendingInCategoryChart.js
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react';
-import { useAuthFetch } from '../hooks';
-import { FormRow, PieChartComponent } from './index';
+import { useAuthFetch, useAlert } from '../hooks';
+import { FormRow, PieChartComponent, Alert } from './index';
 import { DateTime } from 'luxon';
 import { THIS_MONTH, getMultipleDatesRange } from '../utils/dateTime';
 import Wrapper from '../assets/Wrapper/SpendingInCategoryChartWrapper';
@@ -21,6 +21,9 @@ const SpendingInCategoryChart = () => {
   const { isLoading, input_from, input_to } = values;
   const { incurred_on_from, incurred_on_to } = date;
   const authFetch = useAuthFetch();
+
+  const [alert, displayAlert] = useAlert();
+  const { showAlert, alertText, alertType } = alert;
 
   const handleChange = (e) => {
     const { name, value } = e.target;
@@ -49,11 +52,16 @@ const SpendingInCategoryChart = () => {
       const newFrom = new Date(input_from.split('-').join(','));
       const newTo = new Date(input_to.split('-').join(','));
       const { from, to } = getMultipleDatesRange(newFrom, newTo);
-      setDate({
-        incurred_on_from: from,
-        incurred_on_to: to,
-      });
-      fetchCategorizeSpending(from.toISOString(), to.toISOString());
+
+      if (from > to) {
+        displayAlert('End date must be later than start date', 'danger');
+      } else {
+        setDate({
+          incurred_on_from: from,
+          incurred_on_to: to,
+        });
+        fetchCategorizeSpending(from.toISOString(), to.toISOString());
+      }
     }
   };
 
@@ -65,7 +73,7 @@ const SpendingInCategoryChart = () => {
   return (
     <Wrapper>
       <h2>Expenses in categories</h2>
-
+      {showAlert && <Alert alertText={alertText} alertType={alertType} />}
       <form action='' onSubmit={handleSubmit}>
         <FormRow
           handleChange={handleChange}
