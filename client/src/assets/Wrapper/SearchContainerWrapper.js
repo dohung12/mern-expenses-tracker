0381f9ae42b05bfaf469002173f970f3858c9941
dohung <dohuyhung12@gmail.com>
2022-06-26 22:41:24 +0700

    specify capitalize select

--- client/src/assets/Wrapper/SearchContainerWrapper.js
+++ client/src/assets/Wrapper/SearchContainerWrapper.js
@@ -5,7 +5,9 @@ const Wrapper = styled.details`
   box-shadow: 0 10px 15px -3px rgb(0 0 0 / 0.1), 0 4px 6px -4px rgb(0 0 0 / 0.1);
   border-radius: 5px;
   margin-bottom: 2rem;
-  text-transform: capitalize;
+  select {
+    text-transform: capitalize;
+  }
 
   summary {
     border-bottom: 2px solid var(--primary);
