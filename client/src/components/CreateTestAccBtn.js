8b274bb07a70ce8d60f06b2412a563a83243594d
dohung <dohuyhung12@gmail.com>
2022-06-26 22:24:44 +0700

    add navigate when complete loading

--- client/src/components/CreateTestAccBtn.js
+++ client/src/components/CreateTestAccBtn.js
@@ -1,14 +1,19 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import axios from 'axios';
 import { useSetupUser } from '../hooks/index';
+import { useAppContext } from '../context/appContext';
+import { useNavigate } from 'react-router-dom';
+
 const CreateTestAccBtn = () => {
   const [isLoading, setIsLoading] = useState(false);
+  const { state } = useAppContext();
   const setupUser = useSetupUser();
+  const navigate = useNavigate();
+
   const handleClick = async () => {
     setIsLoading(true);
-
     try {
-      const { data } = await axios.get('/auth/test');
+      const { data } = await axios.post('/api/v1/auth/test');
       setupUser(data);
     } catch (error) {
       console.log(error);
@@ -16,11 +21,17 @@ const CreateTestAccBtn = () => {
     setIsLoading(false);
   };
 
+  useEffect(() => {
+    if (state.user) {
+      navigate('/');
+    }
+  }, [state.user, navigate]);
+
   return (
     <button
       disabled={isLoading}
       aria-busy={isLoading}
-      className=''
+      className='secondary '
       onClick={handleClick}
     >
       Create Test Account
